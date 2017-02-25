29a344c036f1c1e48589230a83315a47d83e77fd
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-02-24 21:39:38 -0800

    basic search filter

--- app/view/search-controller.js
+++ app/view/search-controller.js
@@ -6,11 +6,6 @@ function SearchController($log, $scope, searchService) {
 
   $scope.genders = [];
 
-  function genderObj (gender, count) {
-    this.gender = gender;
-    this.count = count;
-  }
-
   $scope.$watch('searchTerm', function() {
     if($scope.searchTerm === '') {
       $scope.searchResults.count = 0;
@@ -19,10 +14,13 @@ function SearchController($log, $scope, searchService) {
     searchService.getPeople($scope.searchTerm)
     .then( people => {
       $scope.searchResults = people;
-      $scope.genders = $scope.searchResults.results.map( person => {
-        return new genderObj(person.gender, 1);
-        // return person.gender;
+      let gendersArray = $scope.searchResults.results.map( person => {
+        return person.gender;
       });
+      $scope.genders = gendersArray.filter( (person, idx) => {
+        return gendersArray.indexOf(person) == idx;
+      });
+      return $scope.genders;
     });
   });
 

d77820b46b5149be3dd45a7dcdcc14c756d22ea9
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-02-24 19:58:04 -0800

    add search filter

--- app/view/search-controller.js
+++ app/view/search-controller.js
@@ -4,12 +4,33 @@ module.exports = ['$log', '$scope', 'searchService', SearchController];
 
 function SearchController($log, $scope, searchService) {
 
-  this.searchResults = [];
+  $scope.genders = [];
+
+  function genderObj (gender, count) {
+    this.gender = gender;
+    this.count = count;
+  }
 
   $scope.$watch('searchTerm', function() {
+    if($scope.searchTerm === '') {
+      $scope.searchResults.count = 0;
+      return;
+    }
+    searchService.getPeople($scope.searchTerm)
+    .then( people => {
+      $scope.searchResults = people;
+      $scope.genders = $scope.searchResults.results.map( person => {
+        return new genderObj(person.gender, 1);
+        // return person.gender;
+      });
+    });
+  });
+
+  $scope.runSearch = function() {
     searchService.getPeople($scope.searchTerm)
     .then( people => {
       $scope.searchResults = people;
     });
-  });
+  };
+
 }
